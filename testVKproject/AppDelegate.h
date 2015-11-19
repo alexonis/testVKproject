34d48cfd905b143e554486bf73db35c9101a6a85
Alexonis <Alexonis@macbook-pro-aleksandr.local>
2015-11-20 00:37:08 +0300

    Initial Commit

---
+++ testVKproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testVKproject
+//
+//  Created by Alexonis on 20.11.15.
+//  Copyright © 2015 Alexonis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testVKproject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testVKproject
+//
+//  Created by Alexonis on 20.11.15.
+//  Copyright © 2015 Alexonis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
